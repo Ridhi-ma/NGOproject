2df8127c4784938d2ce3492e4e4802ee747e951e
Ridhima Mehrotra <rm8970@srmist.edu.in>
2022-05-08 22:54:23 +0530

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 "use strict";
-const nav = document.querySelector(".topnav");
-window.onscroll = () => {
-  const scrolled = window.scrollY;
-  if (scrolled) {
-    nav.documentElement.style.setProperty("orange");
+// let count = 0;
+// const counter = document.querySelector(".counter");
+
+// setTimeout(() => {
+//   counter.textContent = count;
+// }, 1000);
+// setTimeout();
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("topnav").style.top = "0";
+  } else {
+    document.getElementById("topnav").style.top = "-1000px";
   }
+  prevScrollpos = currentScrollPos;
 };
